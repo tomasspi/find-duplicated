d66953cfa187a6eda4722bb6336b69b7f52847e6
Tomas <tom-300@hotmail.com>
2020-05-17 17:16:39 -0300

    find encuentra todos los txt

--- find_duplicate_nohash.pl
+++ find_duplicate_nohash.pl
@@ -11,39 +11,50 @@ my @result = ();
 # find_duplicated(@dir_list)
 sub find_duplicated(@)
 {
-    my @dir_list = @_;
-    if ($#dir_list < 0) {
-        return (undef);
-    }
+  my @dir_list = @_;
+  if ($#dir_list < 0)
+  {
+    return (undef);
+  }
 
-    my @files; 
-    
-    # Here is the list of files under the the @dir_list array
-    # Polulate @files here.
+  my @files;
 
-    # My MD5 Hash
-    my %md5;	
-    # Loop on what we find by md5
-    foreach my $file (@files){
-       next if $file =~ /^\.\.?$/;
-       open(FILE, $file) or next;
-       binmode(FILE);
-       push @{$md5{Digest::MD5->new->addfile(*FILE)->hexdigest}}, $file;
-       close (FILE);
+  # Here is the list of files under the the @dir_list array
+  # Polulate @files here.
+  find(sub
+      {
+        push @files,$_ if (-f $_ and /\.txt$/);
+      }, @dir_list);
+
+  # My MD5 Hash
+  my %md5;
+  # Loop on what we find by md5
+  foreach my $file (@files)
+  {
+    next if $file =~ /^\.\.?$/;
+    open(FILE, $file) or next;
+    binmode(FILE);
+    push(@{ $md5 {Digest::MD5->new->addfile(*FILE)->hexdigest}}, $file);
+    close(FILE);
+  }
+
+  foreach my $hash(keys %md5)
+  {
+  	if( $#{$md5{$hash}} >= 1 )
+    {
+      push(@result, [@{$md5{$hash}}]);
     }
-    foreach my $hash (keys %md5){
-    	if ( $#{$md5{$hash}} >= 1 ) {
-            push (@result, [@{$md5{$hash}}]);
-        }
-    }
-    return @result
+  }
+  return @result
 }
 
 my @duplicated_files = find_duplicated(@ARGV);
 
-foreach my $cur_dump (@duplicated_files){
-    print("Duplicated files:\n");
-    foreach my $curl_file (@$cur_dump){
-        print ("\t$curl_file\n");
-    }
+foreach my $cur_dump (@duplicated_files)
+{
+  print("Duplicated files:\n");
+  foreach my $curl_file (@$cur_dump)
+  {
+    print ("\t$curl_file\n");
+  }
 }
