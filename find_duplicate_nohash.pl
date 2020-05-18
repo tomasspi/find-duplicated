379f18cad0645916b879fdc91b0638fbcd296599
Tomas <green@virma.localdomain>
2020-05-17 23:08:51 -0300

    listo para testings

--- find_duplicate_nohash.pl
+++ find_duplicate_nohash.pl
@@ -8,43 +8,48 @@ use Digest::MD5;
 # Store all results
 my @result = ();
 
+# If no args are passed, searches
+# the current directory
+if ($#ARGV == -1)
+{
+	@ARGV = ".";
+}
+
+
 # find_duplicated(@dir_list)
 sub find_duplicated(@)
 {
-  my @dir_list = @_;
-  if ($#dir_list < 0)
-  {
-    return (undef);
-  }
+	my @dir_list = @_;
+	if($#dir_list < 0)
+	{
+		return (undef);
+	}
+	
+	# Here is the list of files under the the @dir_list array
+   # Polulate @files here.
+	my @files;
+	find(sub{-f && push @files, $File::Find::name}, @dir_list);
+	
+	# My MD5 Hash
+   my %md5;
+   # Loop on what we find by md5
+   foreach my $file (@files)
+   {
+   	my $size = (stat($file))[7];
+	   next unless $size > 1;
+      open(FILE, $file) or next;
+    	binmode(FILE);
+    	push(@{ $md5{Digest::MD5->new->addfile(*FILE)->hexdigest}}, $file);
+    	close(FILE);
+  	}
 
-  my @files;
-
-  # Here is the list of files under the the @dir_list array
-  # Polulate @files here.
-  find(sub
-      {
-        push @files,$_ if (-f $_ and /\.txt$/);
-      }, @dir_list);
-
-  # My MD5 Hash
-  my %md5;
-  # Loop on what we find by md5
-  foreach my $file (@files)
-  {
-    next if $file =~ /^\.\.?$/;
-    open(FILE, $file) or next;
-    binmode(FILE);
-    push(@{ $md5 {Digest::MD5->new->addfile(*FILE)->hexdigest}}, $file);
-    close(FILE);
-  }
-
-  foreach my $hash(keys %md5)
-  {
-  	if( $#{$md5{$hash}} >= 1 )
-    {
-      push(@result, [@{$md5{$hash}}]);
-    }
-  }
+  	foreach my $hash(keys %md5)
+  	{
+  		if( $#{$md5{$hash}} >= 1)
+    	{
+      	push(@result, [@{$md5{$hash}}]);
+    	}
+  	}
   return @result
 }
 
@@ -58,3 +63,6 @@ foreach my $cur_dump (@duplicated_files)
     print ("\t$curl_file\n");
   }
 }
+
+
+	
